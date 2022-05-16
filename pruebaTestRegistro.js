83d591cb393af0a86deb8efd744b38b99957680d
missaelcv <missaelcv0220@gmail.com>
2022-05-16 17:02:06 -0400

    nuevo Credenciales

--- pruebaTestRegistro.js
+++ pruebaTestRegistro.js
@@ -5,24 +5,23 @@ const chrome = require('selenium-webdriver/chrome');
 let options = new chrome.Options();
 let driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();
 
-async function iniciarSesion() {
+async function ejecutarPruebas() {
    describe('Pruebas de integracion', async function() {
     
-      // it('1- 	A intentar iniciar sesión con credenciales no válidas, el sistema lo notifica y no permite el acceso.',async function() {
-      //    this.timeout(30000);
-      //    iniciarSesion();
-      //    await driver.get('http://127.0.0.1:5500/registrodeClientes.html');
+      it('1- 	A intentar iniciar sesión con credenciales no válidas, el sistema lo notifica y no permite el acceso.',async function() {
+         this.timeout(50000);
+       
+         await driver.get('http://127.0.0.1:5500/registrodeClientes.html');
          
-          var username = await driver.findElement(By.id('username'));
-          var password = await driver.findElement(By.password('password'));
-          var bottonLogin = await driver.findElement(By.id('buttonLogin'));
+          var username = await driver.findElement(By.id('usuario'));
+          var password = await driver.findElement(By.id('password'));
+          var botonIniciarSesion = await driver.findElement(By.id('botonIniciarSesion'));
    
           await username.clear();
           await password.clear();
-          await username.sendKey('JuanPerez');
-          await password.sendKey('123456');
- 
-          //buttonLogin.click();
+          await username.sendKeys('JuanPerez');
+          await password.sendKeys('123456');
+          botonIniciarSesion.click();
 
          await driver.sleep(1000);
       
@@ -31,21 +30,23 @@ async function iniciarSesion() {
           it('2-Al intentar iniciar sesión con credenciales válidas, el sistema permite el acceso.',async function() {
             this.timeout(3000);
              await driver.get('http://127.0.0.1:5500/registrodeClientes.html');
-              iniciarSesion();
-             });
+             
+             
                        
-               var username = await driver.findElement(By.id('username'));
-               var password = await driver.findElement(By.password('password'));
-               var buttonLogin = await driver.findElement(By.id('buttonLogin'));
-               var validaCredenciales = await driver.findElement(By.xpath("//strong[contains( text (),'Credenciales no Validos')]"));
+               var username = await driver.findElement(By.id('usuario'));
+               var password = await driver.findElement(By.id('password'));
+               var botonIniciarSesion = await driver.findElement(By.id('botonIniciarSesion'));
+              
                
-               await username.sendKey('JuanPerez');
-               await password.sendKey('1234');
-               buttonLogin.click();
+               await username.sendKeys('JuanPerez');
+               await password.sendKeys('1234');
+               botonIniciarSesion.click();
 
                await driver.sleep(1000);
 
-              console.log(validaCredenciales);
+               var validaCredenciales = await driver.findElement(By.xpath("//strong[contains( text (),'Credenciales no válidos')]"));
+              
+              assert.equal(await validaCredenciales.isDisplayed(),true);
 
 
             // it('3-Al Intentar iniciar sesion con credenciales no validas.',async function() {
@@ -54,4 +55,7 @@ async function iniciarSesion() {
             //    iniciarSesion();
             //    });
             //    await quit();
+            });
+         });
 }
+ejecutarPruebas();
